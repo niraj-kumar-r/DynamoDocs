8cb3900c7bd8229770895244a3ee689bf8f31667
Niraj Kumar <nirajk23523@gmail.com>
2024-04-24 03:34:22 +0530

    dynamically load prompt

--- dynamodocs/__main__.py
+++ dynamodocs/__main__.py
@@ -8,12 +8,15 @@ def main():
     argparser = argparse.ArgumentParser(
         description="Automatic documentation generator")
 
+    argparser.add_argument("-p", "--profile", type=str,
+                           default="dev", help="Choose the prompt profile to use, can be configured in the config file, uses 'dev' by default")
+
     argparser.add_argument("-c", "--clear", action="store_true",
                            help="Clear the output directory before generating the documentation from scratch")
 
     args = argparser.parse_args()
 
-    runner = Runner(clear=args.clear)
+    runner = Runner(clear=args.clear, profile=args.profile)
 
     runner.run()
 
